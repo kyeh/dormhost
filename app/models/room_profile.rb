81e0c59912ad12c27b10579d0bf1fb338a8199bc
Alexander Kowalski <akowalsk@andrew.cmu.edu>
2009-03-02 16:37:53 -0500

    beginning of room and room profile stuff

--- app/models/room_profile.rb
+++ app/models/room_profile.rb
@@ -4,4 +4,19 @@ class RoomProfile < ActiveRecord::Base
   has_many :room_images
   has_many :room_profile_days
   
+  
+  attr_accessible :type, :bed_type, :max_capacity, :pets, :food, :roommates, :internet, :on_campus, :cost, :notes
+  
+  def is_updatable_by(user)
+    user.is_admin?
+  end
+  
+  def self.is_admin?
+    self.is_admin
+  end
+  
+  def self.smoker?
+    self.smoker
+  end
+  
 end
