f7a41d1690c63d9822ae23e3238ca3f997d973e9
makki <makki@andrew.cmu.edu>
2009-03-16 18:22:05 -0400

    creates a host and renter record for a user upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,11 +22,23 @@ class UsersController < ApplicationController
         # button. Uncomment if you understand the tradeoffs.
         # reset session
         self.current_user = @user # !! now logged in
-        redirect_to('/login')
-        flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
-      else
-        flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
-        render :action => 'new'
+        
+        @host = Host.new
+        @host.user_id = @user.id
+        
+        @renter = Renter.new
+        @renter.user_id = @user.id
+        
+        success = @host.save && @renter.save
+        
+        if success && @host.errors.empty? && @renter.errors.empty?
+          
+          redirect_to('/login')
+          flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
+        else
+          flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
+          render :action => 'new'
+        end
       end
     else
       flash[:error] = "Error verifying with recaptcha please enter the 2 words again and re-enter your password before submission"
