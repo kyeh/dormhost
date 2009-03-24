110688aba52b8048058db168cf2d7927f6eea7e8
makki <makki@andrew.cmu.edu>
2009-03-24 17:59:06 -0400

    relationships omitted or corrected

--- app/models/modification.rb
+++ app/models/modification.rb
@@ -1,5 +1,8 @@
 class Modification < ActiveRecord::Base
-  belongs_to :renter
-  belongs_to :host
-  belongs_to :transaction
+  #belongs_to :renter
+  #belongs_to :host
+  puts "GETS TO MODIFICATION\n"
+  #belongs_to :transaction
+  #validates_presence_of :transaction_id, :comments, :check_in, :check_out, :guests, :approved
+
 end
