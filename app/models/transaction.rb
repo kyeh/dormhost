49dadba9e71de85ffd647d4de141222bc4e47785
makki <makki@andrew.cmu.edu>
2009-03-24 17:59:19 -0400

    relationships omitted or corrected

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,8 @@
 class Transaction < ActiveRecord::Base
   has_many :modifications
   belongs_to :renter
-  belongs_to :host
+  #belongs_to :host   #changed by megha
+  belongs_to :room    #changed by megha
+  puts 'GETS TO TRANSACTION \n'
   
 end
