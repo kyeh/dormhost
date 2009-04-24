6d63aa87766574691478fc702c75fcd49ac8202d
makki <makki@andrew.cmu.edu>
2009-04-24 19:53:29 -0400

    view all user reviews for a user, link from rooms/show

--- app/models/room_review.rb
+++ app/models/room_review.rb
@@ -3,4 +3,6 @@ class RoomReview < ActiveRecord::Base
   #belongs_to :transaction  #This relationship messes up creating a new room review
   
   #acts_as_rateable
+  
+  validates_presence_of :rating, :review
 end
--- app/models/user_review.rb
+++ app/models/user_review.rb
@@ -1,5 +1,7 @@
 class UserReview < ActiveRecord::Base
   belongs_to :profile
   
-  acts_as_rateable
+  validates_presence_of :rating, :review
+  
+  #acts_as_rateable
 end
